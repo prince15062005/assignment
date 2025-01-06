36574a10edd1042af2e70032958ee662f081285c
princ <princ@DESKTOP-3HHH3FJ>
2025-01-06 12:56:57 +0530

    Add project files.

---
+++ PositivOrNegitiv.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    internal class PositivOrNegitiv
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a number: ");
+            n = Int32.Parse(Console.ReadLine());
+
+            if(n>0)
+            {
+                Console.WriteLine(n + "is positive");
+                
+            }
+        }
+    }
+}
